2ec65935401c97cf217031c1133bea0c29b97793
Eric Masseran <rico.masseran@gmail.com>
2016-05-25 01:13:56 +0200

    Add a function for getting bss result on a set of samples

--- source_separation/filter/filter.py
+++ source_separation/filter/filter.py
@@ -31,3 +31,28 @@ def get_bss_result(s1, s2, es1, es2):
                         np.array( [es1[1:size], es2[1:size]] ))
 
     return sdr, sir, sar
+
+def batch_bss_result(s1, s2, es1, es2):
+    """
+    Compute BSS stat on a set of samples
+    input
+    matrix nxm with n number of samples and m length
+    return
+    vector of len n for 
+    sdr, sir, sar
+    """
+    n = s1.size
+    sdr = []
+    sir = []
+    sar = []
+
+    for i in range(n):
+        t1, t2, t3 = get_bss_result( s1[i], s2[i], es1[i], es2[i] )
+        sdr.append(t1)
+        sir.append(t2)
+        sar.append(t3)
+
+    return np.array(sdr), np.array(sir), np.array(sar)
+
+
+
