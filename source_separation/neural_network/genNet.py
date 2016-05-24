02d355b6da3ee3ccff00c887931dddbe03cd383a
Corentin Abgrall <abgrall.corentin@gmail.com>
2016-05-24 19:04:37 +0200

    save and load the nets

--- source_separation/neural_network/genNet.py
+++ source_separation/neural_network/genNet.py
@@ -1,24 +1,46 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue May 24 14:05:14 2016
+# Need to download python's library h5py
+# for the save/load models
+# http://docs.h5py.org/en/latest/build.html
 
-@author: flac
-"""
 
-from keras.models import Sequential 
-from keras.layers import Dense, SimpleRNN , Activation
+import numpy as np
+from keras.models import Sequential, model_from_json
+from keras.layers import SimpleRNN, Dense
 from keras.utils.visualize_util import plot
-
 
 
 def genRNN(insize, hidden_layer, outsize, timesteps):
     model = Sequential()
     model.add(SimpleRNN(outsize,input_shape=(timesteps, insize),return_sequences=True, activation='relu'))
     for i in range(hidden_layer):
-        model.add(SimpleRNN(outsize,return_sequences=True, activation = 'relu'))
-    #Ajouter la dernière couche de Marc
+        model.add(SimpleRNN(outsize, return_sequences=True, activation = 'relu'))
+    model.add(SimpleRNN(outsize, return_sequences=False, activation = 'relu'))
+    model.add(Dense(outsize))
     return model
+    
+def printStructure(model, name='model'):
+    plot(model, name + '.png', show_shapes = True)
+    
+def save(model, name, overwrite = False):
+    json_string = model.to_json()
+    open(name + '.json', 'w').write(json_string)
+    model.save_weights(name + 'weights.h5', overwrite)
+    
+def load(name):
+    m = model_from_json(open(name + '.json').read())
+    m.load_weights(name + '_weights.h5')
+    return m
+    
+def genDumbData():
+    X = np.ones((100,2))
+    Y = np.ones(100)
+    return X, Y
+    
+def load_data(data, n_prev = 3):  
+    docX = []
+    for i in range(len(data)-n_prev):
+        docX.append(data[i:i+n_prev,:])
+    return np.array(docX)
 
-m = genRNN(40,3,40,2)
-l = m.layers[0]
-plot(m, 'model.png',show_shapes = True)
+
+
