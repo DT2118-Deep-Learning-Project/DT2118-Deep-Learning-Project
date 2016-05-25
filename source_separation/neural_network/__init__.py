4d616d854397f17e649bdeeb31eb0b56c7d4d628
Eric Masseran <rico.masseran@gmail.com>
2016-05-25 12:10:27 +0200

    Add read input for nn

--- source_separation/neural_network/__init__.py
+++ source_separation/neural_network/__init__.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python
 
-__all__ = [ 'filemodel', 'loss_function' ] 
+__all__ = [ 'filemodel', 'loss_function', 'dummy_dnn', 'mask_data_callback', 'output_layer', 'RNN'] 
--- source_separation/test.py
+++ source_separation/test.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python
+# source_separation.py
+# 
+import source_separation.gen_input.filefeatures as sgf
 
 def train_srs():
     # Load train set    
-
-
+    X = sgf.extract_fft(sgf.load_set_features("../data/features/tidigits_noisy/train", 0), 1)
+    clean = sgf.extract_fft(sgf.load_set_features("../data/features/tidigits_clean/train", 0), 1)
+    noise = sgf.extract_fft(sgf.load_set_features("../data/features/tidigits_noise/train", 0), 1)
+    Y = np.append(clean, noise, axis=0)
     # Create net
     rnn = neural_network.RNN(INPUT_SIZE, 2, noisy, 2, loss=source_separation_loss_function)
 
@@ -19,7 +24,10 @@ def train_srs():
 
 def test_srs():
     # Load test set
-    X = 2
+    X = sgf.extract_fft(sgf.load_set_features("../data/features/tidigits_noisy/test", 0), 1)
+    clean = sgf.extract_fft(sgf.load_set_features("../data/features/tidigits_clean/test", 0), 1)
+    noise = sgf.extract_fft(sgf.load_set_features("../data/features/tidigits_noise/test", 0), 1)
+    Y = np.append(clean, noise, axis=0)
     # Load net
 
     # Test net
