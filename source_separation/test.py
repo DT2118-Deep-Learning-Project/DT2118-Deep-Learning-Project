43a8221d807cb35850c6daa9d6c02881fbec39ec
Eric Masseran <rico.masseran@gmail.com>
2016-05-25 13:25:59 +0200

    Adapt test.py for working on pdc

--- source_separation/test.py
+++ source_separation/test.py
@@ -2,18 +2,23 @@
 # source_separation.py
 # 
 import source_separation.gen_input.filefeatures as sgf
+import numpy as np
+from source_separation.neural_network import *
+
+setsize=10000
+INPUTSIZE=1024
 
 def train_srs():
     # Load train set    
     X = sgf.extract_fft(sgf.load_set_features("../data/features/tidigits_noisy/train", 0), 1)
     clean = sgf.extract_fft(sgf.load_set_features("../data/features/tidigits_clean/train", 0), 1)
     noise = sgf.extract_fft(sgf.load_set_features("../data/features/tidigits_noise/train", 0), 1)
-    Y = np.append(clean, noise, axis=0)
+    Y = np.append(clean[:setsize], noise[:setsize], axis=0)
     # Create net
     rnn = neural_network.RNN(INPUT_SIZE, 2, noisy, 2, loss=source_separation_loss_function)
 
     # Train net
-    rnn.fit(noisy, target)
+    rnn.fit(X[:setsize], Y)
 
     # Save net
     rnn.save()
