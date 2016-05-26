515acf4655f0a6e898a781a39d13cf7826171cf8
Marc Beillevaire <marc.beillevaire@gmail.com>
2016-05-26 17:35:15 +0200

    Adding fft to wav functions

--- source_separation/gen_input/invfeatures.py
+++ source_separation/gen_input/invfeatures.py
@@ -7,13 +7,19 @@ Created on Wed May 25 18:40:45 2016
 
 import scipy.fftpack
 import numpy as np
-from filefeature.py
 
-data = np.load('1b_fft.npz')
+def istft(X, fs, T, hop=0.010):
+     x = scipy.zeros(T*fs)
+     framesamp = X.shape[1]
+     hopsamp = int(hop*fs)
+     for n,i in enumerate(range(0, len(x)-framesamp, hopsamp)):
+         x[i:i+framesamp] += scipy.real(scipy.ifft(X[n]))
+     return x
 
-
-def fromffttowav(data):
-    res = scipy.fftpack.ifft(data)
-    
-    return res
+def fromffttowav(FFTs):
+    fs = 20000
+    hop = 0.01
+    T = FFTs.shape[0] * hop
+    samples = istft(FFTs, 20000, T, hop)
+    return samples
     
--- source_separation/test.py
+++ source_separation/test.py
@@ -2,31 +2,50 @@
 # source_separation.py
 # 
 import gen_input.filefeatures as sgf
+import gen_input.invfeatures as invf
 import numpy as np
 import neural_network.RNN
 import neural_network.loss_function
 
-setsize=10
+setsize=150
 prefix="../data/"
 hidden_layer = 2
 nodes = 150
 activation='relu'
 
-def train_srs():
-    # Load train set    
-    print 'Loading files'
-    X = sgf.extract_fft(sgf.load_set_features(prefix + "features/tidigits_noisy/train", 0), 1)
-    clean = sgf.extract_fft(sgf.load_set_features(prefix + "features/tidigits_clean/train", 0), 1)
-    noise = sgf.extract_fft(sgf.load_set_features(prefix + "features/tidigits_noise/train", 0), 1)
+def load_dataset(datatype):
+    if datatype != 'train' or datatype != 'test':
+        raise Exception('Please pass parameter "train" or "test" to the load data function')
+
+    X     = sgf.extract_fft(sgf.load_set_features(prefix + "features/tidigits_noisy/" + datatype, 0), 1)
+    clean = sgf.extract_fft(sgf.load_set_features(prefix + "features/tidigits_clean/" + datatype, 0), 1)
+    noise = sgf.extract_fft(sgf.load_set_features(prefix + "features/tidigits_noise/" + datatype, 0), 1)
 
     X = X[:setsize]
     Y = np.append(clean[:setsize], noise[:setsize], axis=1)
 
+    # Scaling to get values easier to handle
     scale = np.mean(Y)
-    Y = Y / scale
-    X = X / scale
+    Y     = Y / scale
+    X     = X / scale
 
     input_size = X.shape[1]
+    return X, Y
+
+def save_to_wav(name, stft):
+    '''
+        :param stft: must be an 2D-array of size (N_windows, STFT_length)
+    '''
+    samples = invf.fromffttowav(data)
+    fs      = 20000
+    path    = '../data/output' + name
+    print "Saving wave file to: ", path
+    scipy.io.wavfile.write(path, fs, samples)
+
+def train_srs():
+    # Load train set    
+    print 'Loading files'
+    X, Y = load_dataset('train')
 
     # Create net
     print 'Building RNN'
@@ -43,15 +62,14 @@ def train_srs():
     
     # Result
     print 'LOL'
+    return rnn
 
 
 def test_srs():
     # Load test set
-    X = sgf.extract_fft(sgf.load_set_features(prefix + "features/tidigits_noisy/test", 0), 1)
-    clean = sgf.extract_fft(sgf.load_set_features(prefix + "features/tidigits_clean/test", 0), 1)
-    noise = sgf.extract_fft(sgf.load_set_features(prefix + "features/tidigits_noise/test", 0), 1)
+    print 'Loading files'
+    X, Y = load_dataset('test')
 
-    Y = np.append(clean, noise, axis=0)
     # Load net
 
     # Test net
@@ -59,6 +77,10 @@ def test_srs():
     # Save output result
 
     # Result
-
 print 'Train srs'
-train_srs()
+rnn = train_srs()
+
+X = sgf.extract_fft(sgf.load_set_features(prefix + "features/tidigits_noisy/test", 0), 1)
+pred = rnn.predict(rnn.prepare_data(X[:150]))
+
+
