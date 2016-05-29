1da7e6bdd3522b6f3695aee8529e28610b160f0c
Eric Masseran <eric.masseran@telecom-paristech.fr>
2016-05-29 18:11:03 +0200

    Rename test.py to train.py

--- source_separation/__init__.py
+++ source_separation/__init__.py
@@ -2,7 +2,7 @@
 """Top-level module for source separation"""
 
 # Import all submodules 
-#import test
-#import neural_network
-#import preprocess
-from . import *
+import test
+import neural_network
+import preprocess
+#from . import *
---
+++ source_separation/train.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+# source_separation.py
+# 
+import numpy as np
+import os
+import scipy
+import neural_network.DNN as DNN
+import neural_network.loss_function
+from preprocess import dataIO
+
+setsize = 30# Number of tidigits to take in the set, choose 0 to take all of them
+nb_epoch=3
+prefix = os.environ['SPEECH_PROJ_DIR']+"/data/"
+hidden_layer = 4
+nodes = 450
+activation='relu'
+
+def train_srs():
+    # Load train set    
+    print 'Loading files'
+    X, Y = dataIO.train_set(setsize)
+
+    X = scipy.absolute(X)
+    Y = scipy.absolute(Y)
+
+    scale = np.mean(X)
+    print("Scale: " + str(scale))
+    Y = Y / scale
+    X = X / scale
+
+    # Create net
+    print 'Building RNN'
+    rnn = DNN.DNN(512, hidden_layer, nodes, X,
+            loss=neural_network.loss_function.source_separation_loss_function, activation=activation)
+
+    # Train net
+    print 'Training'
+    rnn.fit(X, Y, nb_epoch=nb_epoch, batch_size=100)
+
+    # Save net
+    print 'Saving'
+    rnn.save()
+    return rnn
+
+def test_srs(rnn):
+    # Load test set
+    print 'Loading files'
+    X, Y = dataIO.test_set()
+
+    # Load net
+
+    # Test net
+    score = rnn.evaluate(X, Y)
+
+    return score
+
+# Run a train if used as a script
+if __name__ == '__main__':
+    train_srs()
